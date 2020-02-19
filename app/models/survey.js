58308d05082f87a5ab33a8ca81729ddc4adc6a0d
Scott Solmonson <ssolmonson63@gmail.com>
2020-02-19 15:32:35 -0500

    Update survey model
    Added possibleAnswer key to survey model
    
    Co-authored-by: Kayla shalisha.angelique@gmail.com

--- app/models/survey.js
+++ app/models/survey.js
@@ -7,6 +7,10 @@ const surveySchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  possibleAnswers: {
+    type: String,
+    required: true
+  },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
